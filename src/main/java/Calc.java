a847fc686783a6a60fe17fbf6db2c0a81bc50d04
Admin <Admin>
2021-04-13 13:51:36 +0300

    Реализация метода add

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,11 +1,29 @@
 public class Calc {
 
     public int sum (String expression){
+        if(expression == null)
+            return -1;
         if (expression.isEmpty())
             return 0;
+
         String[] numbers = expression.split(",");
         if(numbers.length > 3)
             return -1;
+
+        return getSum(numbers);
+    }
+
+    public int add (String expression){
+        if(expression == null)
+            return -1;
+        if (expression.isEmpty())
+            return 0;
+
+        String[] numbers = expression.split(",");
+        return getSum(numbers);
+    }
+
+    private int getSum(String [] numbers){
         int sum = 0;
         for(String num : numbers) {
             try {
@@ -15,9 +33,5 @@ public class Calc {
             }
         }
         return sum;
-    }
-
-    public int add (String expression){
-        throw new UnsupportedOperationException();
     }
 }
